dcdcd913e388e1c31af7c481d048e2d16f984147
tgile512 <tyler.gile@kellogg.northwestern.edu>
2022-02-03 05:40:53 +0000

    commit

--- code-along/1-models.rb
+++ code-along/1-models.rb
@@ -11,10 +11,36 @@ Company.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
+
 # 2. create new companies
+
+values = { name: "Apple Inc.", url: "https://www.apple.com", city: "Cupertino", state: "CA" }
+apple = Company.new(values)
+apple.save
+
+values = { name: "Amazon.com, Inc.", url: "https://www.amazon.com", city: "Seattle", state: "WA" }
+amazon = Company.new(values)
+amazon.save
+
+new_company = Company.new
+new_company.name = "Tesla, Inc."
+new_company.url = "https://www.tesla.com"
+new_company.city = "Palo Alto"
+new_company.state = "CA"
+new_company.save
+
+p Company.inspect
+
+
 
 # 3. query companies table
 
 # 4. read column values from row
 
 # 5. update attribute value
+
+apple.slogan = "Think Different"
+apple.save
+
+p apple
+
--- labs/1-models.rb
+++ labs/1-models.rb
@@ -11,7 +11,51 @@ Contact.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
+apple = Company.where({ name: "Apple Inc."})[0]
+amazon = Company.where({ name: "Amazon.com, Inc."})[0]
+tesla = Company.where({ name: "Tesla, Inc."})[0]
+
 # 2. create 1-2 new contacts for each company (they can be made up)
+
+values = {
+    first_name: "Tim",
+    last_name: "Cook",
+    email: "tim@apple.com",
+    phone_number: "777-777-7777",
+    company_id: apple.id
+}
+contact = Contact.new(values)
+contact.save
+
+values = {
+    first_name: "Andy",
+    last_name: "Jassy",
+    email: "andy@amazon.com",
+    phone_number: "888-888-8888",
+    company_id: amazon.id
+}
+contact = Contact.new(values)
+contact.save
+
+values = {
+    first_name: "Craig",
+    last_name: "Federighi",
+    email: "craig@apple.com",
+    phone_number: "999-999-9999",
+    company_id: apple.id
+}
+contact = Contact.new(values)
+contact.save
+
+values = {
+    first_name: "Elon",
+    last_name: "Musk",
+    email: "elon@tesla.com",
+    phone_number: "666-666-6666",
+    company_id: tesla.id
+}
+contact = Contact.new(values)
+contact.save
 
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
 
